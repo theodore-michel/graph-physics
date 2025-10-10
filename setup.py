075718bb7bf2d2a77bf05708a1eb2a4380350cde
theodore-michel <theodore.michel@minesparis.psl.eu>
2025-10-10 13:24:07 +0200

    update: setup and gitignore

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="graph-physics",
     version="0.0.1",
+    packages=["graphphysics"],
     entry_points={"console_scripts": ["grph=graphphysics.train:main"]},
 )
